bd5aff7572a59d0627e0d6bca26a96fe6621d963
Duber Alexander Galvis Giraldo - Ceiba Software <duber.galvis@ceiba.com.co>
2022-01-19 14:59:23 -0500

    ajustes pruebas cypress

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -4,59 +4,70 @@
 describe('Pruebas de Login', () => {
 
     beforeEach(() => {
+        cy.fixture('usuario.json').as('datosUsuario');
         cy.visit(`http://localhost:3001/usuario`);
         cy.contains('h2', 'Inicia de Sesión').should('be.visible');
     });
 
     it('Debe registar un Usuario', () => {
-        cy.get('.sc-fznKkj').click();
-        cy.get('[name="nombre"]').type('UsuarioPrueba');
-        cy.get('[name="clave"]').type('1235');
-        cy.get('[name="confirmarClave"]').type('1235');
-        cy.contains('button', 'Registrar').click();
-        cy.wait(2000);
-        cy.get('.sc-fzoLag').should('not.exist');
+        cy.get('@datosUsuario').then((datosUsuario) =>{
+            cy.get('.sc-fznKkj').click();
+            cy.get('[name="nombre"]').type(datosUsuario.usuario);
+            cy.get('[name="clave"]').type(datosUsuario.clave);
+            cy.get('[name="confirmarClave"]').type(datosUsuario.clave);
+            cy.contains('button', 'Registrar').click();
+            cy.wait(2000);
+            cy.get('.sc-fzoLag').should('not.exist');
+        });
     });
 
     it('Debe iniciar sesión el Usuario', () => {
-        cy.get('[name="nombre"]').type('UsuarioPrueba');
-        cy.get('[name="clave"]').type('1235');
-        cy.contains('button', 'Siguiente').click();
-        cy.wait(2000);
-        cy.contains('a', '| Ajustes de Cuenta').should('be.visible');
+        cy.get('@datosUsuario').then((datosUsuario) =>{
+            cy.get('[name="nombre"]').type(datosUsuario.usuario);
+            cy.get('[name="clave"]').type(datosUsuario.clave);
+            cy.contains('button', 'Siguiente').click();
+            cy.wait(2000);
+            cy.contains('a', '| Ajustes de Cuenta').should('be.visible');
+        });
     });
 
     it('Debe fallar con un Usuario erroneo', () => {
-        cy.get('[name="nombre"]').type('UsuarioErroneo');
-        cy.get('[name="clave"]').type('1235');
-        cy.contains('button', 'Siguiente').click();
-        cy.wait(2000);
-        cy.contains('h2', 'Error de Credenciales o Usuario no Existe').should('be.visible');
+        cy.get('@datosUsuario').then((datosUsuario) =>{
+            cy.get('[name="nombre"]').type(datosUsuario.usuario_erroneo);
+            cy.get('[name="clave"]').type(datosUsuario.clave);
+            cy.contains('button', 'Siguiente').click();
+            cy.wait(2000);
+            cy.contains('h2', 'Error de Credenciales o Usuario no Existe').should('be.visible');
+        });
     });
 
     it('Debe de ingresar a ajustes y cambiar la contraseña de Usuario', () => {
-        cy.get('[name="nombre"]').type('UsuarioPrueba');
-        cy.get('[name="clave"]').type('1235');
-        cy.contains('button', 'Siguiente').click();
-        cy.wait(2000);
-        cy.contains('a', '| Ajustes de Cuenta').should('be.visible');
-        cy.contains('a', '| Ajustes de Cuenta').click();
-        cy.get('[name="claveActual"]').type('1235');
-        cy.get('[name="claveNueva"]').type('1235');
-        cy.get('[name="confirmarclaveNueva"]').type('1235');
-        cy.wait(2000);
-        cy.contains('button', 'Actualizar').click();
-        cy.contains('h2', 'Se ha Actualizado su Clave, Inicie Sesión').should('be.visible');
+        cy.get('@datosUsuario').then((datosUsuario) =>{
+            cy.get('[name="nombre"]').type(datosUsuario.usuario);
+            cy.get('[name="clave"]').type(datosUsuario.clave);
+            cy.contains('button', 'Siguiente').click();
+            cy.wait(2000);
+            cy.contains('a', '| Ajustes de Cuenta').should('be.visible');
+            cy.contains('a', '| Ajustes de Cuenta').click();
+            cy.get('[name="claveActual"]').type(datosUsuario.clave);
+            cy.get('[name="claveNueva"]').type(datosUsuario.claveNueva);
+            cy.get('[name="confirmarclaveNueva"]').type(datosUsuario.claveNueva);
+            cy.wait(2000);
+            cy.contains('button', 'Actualizar').click();
+            cy.contains('h2', 'Se ha Actualizado su Clave, Inicie Sesión').should('be.visible');
+        });
     });
 
     it('Debe ingresar a ajustes y dar de Baja al Usuario', () => {
-        cy.get('[name="nombre"]').type('UsuarioPrueba');
-        cy.get('[name="clave"]').type('1235');
-        cy.contains('button', 'Siguiente').click();
-        cy.wait(2000);
-        cy.contains('a', '| Ajustes de Cuenta').should('be.visible');
-        cy.contains('a', '| Ajustes de Cuenta').click();
-        cy.get('[href="/"] > .sc-fzoiQi').click();
-        cy.wait(2000);
+        cy.get('@datosUsuario').then((datosUsuario) =>{
+            cy.get('[name="nombre"]').type(datosUsuario.usuario);
+            cy.get('[name="clave"]').type(datosUsuario.claveNueva);
+            cy.contains('button', 'Siguiente').click();
+            cy.wait(2000);
+            cy.contains('a', '| Ajustes de Cuenta').should('be.visible');
+            cy.contains('a', '| Ajustes de Cuenta').click();
+            cy.get('[href="/"] > .sc-fzoiQi').click();
+            cy.wait(2000);
+        });
     });
 });
--- cypress/integration/pedidoUsuario.spec.js
+++ cypress/integration/pedidoUsuario.spec.js
@@ -3,13 +3,16 @@
 
 describe('Pruebas de Login', () => {
     beforeEach(() => {
-        cy.visit(`http://localhost:3001/usuario`);
-        cy.contains('h2', 'Inicia de Sesión').should('be.visible');
-        cy.get('[name="nombre"]').type('UsuarioPrueba2');
-        cy.get('[name="clave"]').type('1234');
-        cy.contains('button', 'Siguiente').click();
-        cy.wait(2000);
-        cy.contains('a', '| Ajustes de Cuenta').should('be.visible');
+        cy.fixture('pedidos.json').as('datosPedido');
+        cy.get('@datosPedido').then((datosPedido) =>{
+            cy.visit(datosPedido.url);
+            cy.contains('h2', 'Inicia de Sesión').should('be.visible');
+            cy.get('[name="nombre"]').type(datosPedido.usuario);
+            cy.get('[name="clave"]').type(datosPedido.clave);
+            cy.contains('button', 'Siguiente').click();
+            cy.wait(2000);
+            cy.contains('a', '| Ajustes de Cuenta').should('be.visible');
+        });
     });
 
     it('Debe crear un pedido al Usuario', () => {
@@ -17,13 +20,13 @@ describe('Pruebas de Login', () => {
         cy.wait(2000);
         cy.get('[name="producto"]').select('Nombre: Arroz Chino | Precio: 26000');
         cy.get('[name="reunion"]').select('TIPO_MEDIANA | Precio: 50000');
-        cy.get('.react-datepicker__input-container > input').type('January 13, 2022 3:00 PM');
+        cy.get('.react-datepicker__input-container > input').type(' ');
         cy.get('.react-datepicker__day--013').click();
         cy.get('.react-datepicker__time-list-item--selected').click();
         cy.get('[name="direccion"]').type('carrera 80 # 77-44');
         cy.get('[type="number"]').type('5');
         cy.contains('button', 'Hacer Pedido').click();
-        cy.contains('h2', 'Su pedido fue Created').should('be.visible');
+        cy.contains('h2', 'Su pedido fue creado').should('be.visible');
         cy.wait(2000);
     });
 
@@ -37,14 +40,14 @@ describe('Pruebas de Login', () => {
         cy.wait(2000);
         cy.get('a > .sc-fzqBZW').click();
         cy.get('[name="producto"]').select('Nombre: Arroz Chino | Precio: 26000');
-        cy.get('[name="reunion"]').select('TIPO_GRANDE | Precio: 75000');
+        cy.get('[name="reunion"]').select('TIPO_GRANDE | Precio: 100000');
         cy.get('.react-datepicker__input-container > input').click();
         cy.get('.react-datepicker__day--013').click();
         cy.get('.react-datepicker__time-list-item--selected').click();
         cy.get('[name="direccion"]').type('carrera 80 # 77-44');
         cy.get('[type="number"]').type('5');
         cy.contains('button', 'Modificar Pedido').click();
-        cy.contains('h2', 'Su Pedido ha sido modificado OK').should('be.visible');
+        cy.contains('h2', 'Su Pedido ha sido modificado con exito').should('be.visible');
         cy.wait(1500);
         cy.contains('button', 'Regresar').click();
     });
@@ -56,20 +59,17 @@ describe('Pruebas de Login', () => {
         cy.contains('h2', 'Cancelación Exitosa').should('be.visible');
     });
 
-    it('Debe de generar error al solicitar pedido del usuario', () => {
+    it('Debe de generar error al crear pedido un lunes', () => {
         cy.contains('a', '| Ir a Pedidos').click();
         cy.wait(2000);
-        cy.get('a > .sc-fzqBZW').click();
         cy.get('[name="producto"]').select('Nombre: Bandeja Paisa | Precio: 45000');
         cy.get('[name="reunion"]').select('TIPO_PEQUENA | Precio: 25000');
         cy.get('.react-datepicker__input-container > input').click();
-        cy.get('.react-datepicker__day--013').click();
+        cy.get('.react-datepicker__day--024').click();
         cy.get('.react-datepicker__time-list-item--selected').click();
         cy.get('[name="direccion"]').type('carrera 80 # 77-44');
         cy.get('[type="number"]').type('5');
-        cy.contains('button', 'Modificar Pedido').click();
-        cy.contains('h2', 'Su Pedido ha sido modificado OK').should('be.visible');
-        cy.wait(1500);
-        cy.contains('button', 'Regresar').click();
+        cy.contains('button', 'Hacer Pedido').click();
+        cy.contains('h2', 'No se puede agendar pedido para los Lunes').should('be.visible');
     });
 });
